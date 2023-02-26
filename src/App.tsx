15a402d1a06267530d502ee6f5cd213a94e25378
Angelika Tyborska <angelikatyborska@fastmail.com>
2023-02-26 17:35:43 +0100

    Add history extension

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Document from '@tiptap/extension-document'
 import Dropcursor from '@tiptap/extension-dropcursor'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
+import History from '@tiptap/extension-history'
 import { EditorContent, useEditor } from '@tiptap/react'
 import React from 'react'
 import './App.css'
@@ -15,6 +16,7 @@ export default () => {
       Text,
       CustomImage,
       Dropcursor,
+      History
     ],
     content: `
     <p>The image below has no alt text.</p>
